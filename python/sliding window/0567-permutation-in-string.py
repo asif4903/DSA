87f8f50116a579eb2a80c92c93b53d09d517f08d
Mohd Furkan <asif4903@gmail.com>
2023-09-13 12:30:03 +0530

    permutation in string solution added

---
+++ python/sliding window/0567-permutation-in-string.py
@@ -0,0 +1,39 @@
+# leetcode link: https://leetcode.com/problems/permutation-in-string
+
+class Solution:
+    def checkInclusion(self, s1, s2):
+        if len(s1) > len(s2):
+            return False
+
+        s1Count, s2Count = [0] * 26, [0] * 26
+        for i in range(len(s1)):
+            s1Count[ord(s1[i]) - ord("a")] += 1
+            s2Count[ord(s2[i]) - ord("a")] += 1
+
+        matches = sum(map(lambda i: s1Count[i] == s2Count[i], range(26)))
+
+        for l in range(len(s2) - len(s1)):
+            if matches == 26:
+                return True
+
+            i = ord(s2[l]) - ord("a")
+            s2Count[i] -= 1
+            if s2Count[i] == s1Count[i]:
+                matches += 1
+            elif s2Count[i] == s1Count[i] - 1:
+                matches -= 1
+
+            i = ord(s2[l + len(s1)]) - ord("a")
+            s2Count[i] += 1
+            if s2Count[i] == s1Count[i]:
+                matches += 1
+            elif s2Count[i] == s1Count[i] + 1:
+                matches -= 1
+
+        return matches == 26
+
+
+s1 = "ab"
+s2 = "eidbaooo"
+
+print(Solution().checkInclusion(s1, s2))
